5973680abde87e4297c3be6137606ea4bb5a4491
Nicolas Holzinger <nicoholzin@gmail.com>
2024-10-31 17:22:44 -0700

    v2

--- js/Exercice2.js
+++ js/Exercice2.js
@@ -1,71 +1,23 @@
-const houses = [
-  {
-    code: "ST",
-    name: "Stark"
-  },
-  {
-    code: "LA",
-    name: "Lannister"
-  },
-  {
-      code: "BA",
-      name: "Baratheon"
-  },
-  {
-    code: "TA",
-    name: "Targaryen"
-  }
-];
+function lookupUser() {
+  const username = document.getElementById('username').value;
+  const url = `https://api.github.com/users/${username}`;
 
-// Return an array of characters belonging to a house
-const getCharacters = houseCode => {
-    switch (houseCode) {
-      case "ST":
-        return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
-      case "LA":
-        return ["Tywin", "Cersei", "Jaime", "Tyrion"];
-      case "BA":
-        return ["Robert", "Stannis", "Renly"];
-      case "TA":
-        return ["Aerys", "Daenerys", "Viserys"];
-      default:
-        return []; // Empty array
-    }
-};  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const profilePicture = document.getElementById('profilePicture');
+      profilePicture.innerHTML = data.avatar_url 
+        ? `<img src="${data.avatar_url}" alt="Profile Picture">` 
+        : `<img src="default-profile.png" alt="No Picture Available">`;
 
-function dropdownfilling() {
-  //get element
-  const houseSelect = document.getElementById("house");
-  
-  //add all houses to the options
-  houses.forEach(house => {
-    const option = document.createElement("option");
-    option.value = house.code;
-    option.textContent = house.name;
-    houseSelect.appendChild(option);
-  });
-}
-
-function showCharacters() {
-  //get element
-  const houseSelect = document.getElementById("house");
-  const charactersList = document.getElementById("characters");
-
-  //add event listener
-  houseSelect.addEventListener("change", function () {
-    charactersList.innerHTML = "";
-
-    const selectedHouse = houseSelect.value;
-    const characters = getCharacters(selectedHouse);
-
-  //display the list of characters
-    characters.forEach(character => {
-      const li = document.createElement("li");
-      li.textContent = character;
-      charactersList.appendChild(li);
+      document.getElementById('name').textContent = data.name || 'N/A';
+      document.getElementById('blog').innerHTML = data.blog 
+        ? `<a href="${data.blog}" target="_blank">${data.blog}</a>` 
+        : 'N/A';
+      document.getElementById('created').textContent = data.created_at || 'N/A';
+    })
+    .catch(error => {
+      console.error(error.message);
+      
     });
-  });
 }
-
-dropdownfilling();
-showCharacters();
